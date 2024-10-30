0fffc4fe450bcbda86f89689858ed296ec18b78a
Aleksandar M <sale@kolosek.com>
2024-10-30 16:18:52 +0100

    [#188495371] Integrate with Ghost v1

---
+++ app/services/ghost_client.rb
@@ -0,0 +1,28 @@
+class GhostClient
+  HOST_URL = ENV['GHOST_API_URL']
+  CONTENT_API_KEY = ENV['GHOST_CONTENT_API_KEY']
+
+  def get_post(slug)
+    begin
+      resp = HTTParty.get(
+        "#{HOST_URL}/content/posts/slug#{slug}?key=#{CONTENT_API_KEY}&include=tags"
+      )
+
+      JSON.parse(resp.body)['posts'].first
+    rescue => e
+      Rails.logger.error e.message
+    end
+  end
+
+  def get_posts
+    begin
+      resp = HTTParty.get(
+        "#{HOST_URL}/content/posts/?key=#{CONTENT_API_KEY}&include=tags"
+      )
+
+      JSON.parse(resp.body)['posts']
+    rescue => e
+      Rails.logger.error e.message
+    end
+  end
+end
