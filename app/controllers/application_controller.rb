948d4312cce714e2f3ed1bf03e88431a2b3fcd15
SasaV-Kolosek <sasavas@kolosek.com>
2025-11-19 17:20:20 +0100

    Fix ghost timeout- no settings set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :load_scripts
   before_action :store_utm_in_session
+  before_action :set_settings
 
   helper_method :app_name
 
@@ -16,13 +17,16 @@ class ApplicationController < ActionController::Base
   end
 
   def set_settings
-    begin
-      @settings = Rails.cache.fetch("ghost_settings_#{app_name}", expires_in: 1.hour) do
+    @settings = begin
+      Rails.cache.fetch("ghost_settings_#{app_name}", expires_in: 1.hour) do
         ghost_client.get_data(:settings)
       end
     rescue
-      @settings = {}
+      {}
     end
+
+    @settings = {} unless @settings.is_a?(Hash)
+    @settings['lang'] ||= 'en'
   end
 
 	def not_found
@@ -42,8 +46,14 @@ class ApplicationController < ActionController::Base
   private
 
   def load_scripts
-    @header_script = Script.find_by(key: 'header')&.value
-    @footer_script = Script.find_by(key: 'footer')&.value
+    begin
+      @header_script = Script.find_by(key: 'header')&.value
+      @footer_script = Script.find_by(key: 'footer')&.value
+    rescue StandardError => e
+      Rails.logger.error "load_scripts error: #{e.class} #{e.message}"
+      @header_script = nil
+      @footer_script = nil
+    end
   end
 
   def store_utm_in_session
